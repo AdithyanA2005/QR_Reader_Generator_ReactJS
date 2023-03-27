d037ff2d9a5cf7cde805c9b072d5edf718797461
Adithyan A <69440263+AdithyanA2005@users.noreply.github.com>
2023-03-27 05:55:00 +0000

    React Router Dom Added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import QRCode from "react-qr-code";
-import styled from "styled-components";
-
-const Navbar = styled.nav`
-  width: 100%;
-  height: 2rem;
-  background-color: #364156;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar>
-      NavBrand
-    </Navbar>
-    {/* <div> */}
-      {/* // Can be anything instead of `maxWidth` that limits the width. */}
-{/* <div style={{ height: "auto", margin: "0 auto", maxWidth: "20rem", paddingTop: "2rem", width: "100%" }}> */}
-    {/* <QRCode */}
-    {/* size={256} */}
-    {/* style={{ height: "auto", maxWidth: "100%", width: "100%" }} */}
-    {/* value={"adithyan"} */}
-    {/* viewBox={`0 0 256 256`} */}
-    {/* /> */}
-{/* </div> */}
-    {/* </div> */}
-
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<>Home</>} />
+            <Route path="/scan" element={<>Scan</>} />
+            <Route path="/generate" element={<>Generate</>} />
+        </Routes>
+      </BrowserRouter>
 </>
-
   );
 }
 
