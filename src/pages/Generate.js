fe295db3c4f7b932032b84d8d3e5643f0fa5cbb8
Adithyan A <deepaadithyan56@gmail.com>
2023-03-29 16:39:53 +0530

    QrCode Preview and Download option added

--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -1,18 +1,69 @@
-import React from "react";
+import styled from "styled-components";
 import PageContainer from "../components/Containers/PageContainer";
 import Heading from "../components/Headings/Heading";
 import GeneratorInput from "../components/GeneratorInput";
 import SubHeading from "../components/Headings/SubHeading";
 import QrBgWarningQuote from "../components/QrBgWarningQuote";
 export default function Generate() {
+  const [qrValue, setQrValue] = useState("");
+  const handleDownloadBtnClick = () => {
+    const qr_canvas = document.getElementById("qr-canvas");
+    var link = document.createElement('a');
+    link.download = 'adiqr-qrcode.png';
+    link.href = qr_canvas.toDataURL("image/png")
+    link.click();
+    link.remove();
+  }
+
   return (
     <>
       <PageContainer>
         <Heading title="Generate QR-Code" />
         <GeneratorInput value={qrValue} setValue={setQrValue} />
+
         <SubHeading title="Download QR-Code" />
         <QrBgWarningQuote />
+        
+        <DownloadContainer>
+          <QrWrapper>
+            <QRCodeCanvas id="qr-canvas" size={200} includeMargin={true} value={qrValue} />
+          </QrWrapper>
+
+          <ActionWrapper>
+            <DownloadButton onClick={handleDownloadBtnClick}>Download your QR-Code</DownloadButton>
+          </ActionWrapper>  
+        </DownloadContainer>
       </PageContainer>
     </>
   );
 }
+
+const DownloadContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 1.5rem;
+  flex-wrap: wrap;
+`;
+const QrWrapper = styled.div`
+  box-sizing: content-box;
+  height: 200px;
+  width: 200px;
+  border: 2px solid black;
+  margin: auto;
+`;
+const ActionWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+const DownloadButton = styled.button`
+  color: white;
+  background-color: black;
+  cursor: pointer;
+  font-size: 1.2rem;
+  letter-spacing: 1px;
+  padding: 1.2rem 3rem;
+  border: 0;
+  border-radius: 10px;
+`;
