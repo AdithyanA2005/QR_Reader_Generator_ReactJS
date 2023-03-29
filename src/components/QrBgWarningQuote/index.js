dd6ddef2fa6704bd3c00b0a916d35c69c64c19ad
Adithyan A <deepaadithyan56@gmail.com>
2023-03-29 15:51:39 +0530

    Warning BlockQuote component added

---
+++ src/components/QrBgWarningQuote/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function QrBgWarningQuote() {
+  return (
+    <BlockQuote>
+      <em>
+        <b>NOTE:</b>
+        {" "}Make sure to always use QR-Code in bright environment so that the
+        blacks are clearly visible
+      </em>
+    </BlockQuote>
+  );
+}
+
+const BlockQuote = styled.blockquote`
+  color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.05);
+  border-left: 4px solid black;
+  padding: 1.5rem;
+
+  b {
+    color: black;
+  }
+`;
--- src/pages/Generate.js
+++ src/pages/Generate.js
@@ -2,12 +2,16 @@ import React from "react";
 import PageContainer from "../components/Containers/PageContainer";
 import Heading from "../components/Headings/Heading";
 import GeneratorInput from "../components/GeneratorInput";
+import SubHeading from "../components/Headings/SubHeading";
+import QrBgWarningQuote from "../components/QrBgWarningQuote";
 export default function Generate() {
   return (
     <>
       <PageContainer>
         <Heading title="Generate QR-Code" />
         <GeneratorInput value={qrValue} setValue={setQrValue} />
+        <SubHeading title="Download QR-Code" />
+        <QrBgWarningQuote />
       </PageContainer>
     </>
   );
