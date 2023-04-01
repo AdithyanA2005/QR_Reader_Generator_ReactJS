7512b08a54cd55d839f6d073799c070caeaf6258
Adithyan A <deepaadithyan56@gmail.com>
2023-04-01 18:57:42 +0530

    BugFix: Blank space shown insted of qrcode when value is empty

--- src/context/QrSize/QrSizeState.js
+++ src/context/QrSize/QrSizeState.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import QrSizeContext from "./QrSizeContext";
+import { default_qrcode_size } from "../../constants";
 
 export default function QrSizeState(props) {
-  const [qrSize, setQrSize] = useState("");
+  const [qrSize, setQrSize] = useState(default_qrcode_size);
 
   return (
     <QrSizeContext.Provider value={{ qrSize, setQrSize }}>
