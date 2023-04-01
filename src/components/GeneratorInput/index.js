b5daaa578dc555b0b755437b6c38b7f107b1ad99
Adithyan A <deepaadithyan56@gmail.com>
2023-04-01 15:57:34 +0530

    GenerateInput component btn text made a prop

--- src/components/GeneratorInput/index.js
+++ src/components/GeneratorInput/index.js
@@ -2,27 +2,19 @@ import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
-export default function TextInput(props) {
-  const goBtnOnClickHandle  = () => {
+export default function GenerateInput(props) {
+  const goBtnOnClickHandle = () => {
     window.scrollBy(0, 160);
   }
 
   return (
     <ValueInputWrapper>
       <Input type="text" placeholder="Enter QR-Code Value" value={props.value} onChange={(e) => props.setValue(e.target.value)} />
-      <GoBtn onClick={goBtnOnClickHandle}>Go</GoBtn>
+      <GoBtn onClick={goBtnOnClickHandle}>{props.btnText}</GoBtn>
     </ValueInputWrapper>
   );
 }
 
-const Input = styled.input`
-  flex: 1;
-  border: 0;
-  outline: none;
-  padding: 1rem 0.75rem; 
-  font-size: 1.1rem;
-  letter-spacing: 1px;
-`;
 const ValueInputWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -30,6 +22,14 @@ const ValueInputWrapper = styled.div`
   border-radius: 15px;
   background-color: red;
   overflow: hidden;
+`;
+const Input = styled.input`
+  flex: 1;
+  border: 0;
+  outline: none;
+  padding: 1rem 0.75rem; 
+  font-size: 1.1rem;
+  letter-spacing: 1px;
 `;
 const GoBtn = styled.button`
   color: white;
@@ -40,7 +40,12 @@ const GoBtn = styled.button`
   letter-spacing: 1px;
 `;
 
-TextInput.propTypes = {
+GenerateInput.propTypes = {
+  btnText: PropTypes.string,
   value: PropTypes.string.isRequired,
   setValue: PropTypes.func.isRequired,
+};
+
+GenerateInput.defaultProps = {
+  btnText: "Go"
 };
