cab69300fa65f6daa04c4b286329798010ef18a5
Adithyan A <deepaadithyan56@gmail.com>
2023-04-03 00:19:09 +0530

    Homepage responsiveness improved

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,9 @@ export default function Home() {
 
 const SectionContainer = styled.div`
   position: relative;
-  margin-top: 10%;
+  display: flex;
+  justify-content: center;
+  height: 34rem;
 `;
 const HeroBg = styled.img`
   display: block;
@@ -43,27 +45,32 @@ const HeroContent = styled.div`
   position: absolute;
   inset: 0%;
   display: flex;
-  gap: 4rem;
+  gap: 2.5rem;
   flex-direction: column;
   justify-content: center;
   color: black;
   background-color: rgba(255, 255, 255, 0.65);
   backdrop-filter: blur(10px);
+  min-width: 100%;
 `;
 const HeroHeading = styled.h1`
-  font-size: 2.5rem;
+  font-size: 2rem;
   text-align: center;
 
   span {
     font-family: "Orbitron", sans-serif;
   }
 
-  @media only screen and (min-width: 650px) {
-    font-size: 4rem;
+  @media only screen and (min-width: 320px) {
+    font-size: 2.5rem;
   }
 
   @media only screen and (min-width: 475px) {
     font-size: 3rem;
+  }
+
+  @media only screen and (min-width: 650px) {
+    font-size: 4rem;
   }
 `;
 const HeroActions = styled.div`
