fc6cd72e80cef80cf542d9a6108108b4816b980b
Adithyan A <deepaadithyan56@gmail.com>
2023-04-04 13:31:47 +0530

    CSS transition added for action btns and new action btn with a link icon added

--- src/components/QrResultDialog/index.js
+++ src/components/QrResultDialog/index.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import copy from "copy-to-clipboard";
+import validator from "validator";
 import PropTypes from "prop-types";
 import { ReactComponent as CopySVG } from "../../assets/copy-solid.svg";
+import { ReactComponent as LinkSVG } from "../../assets/link-solid.svg";
 import { toast } from "react-toastify";
+
 export default function QrResultDialog(props) {
+  const [isValidURL, setIsValidURL] = useState(false);
   const handleCopyToClipboard = () => {
     copy(props.text);
     toast("🦄 Copied To Clipboard")
   }
 
+
+  useEffect(() => {
+    setIsValidURL(validator.isURL(props.text));
+  }, [props.text])
+
   return (
     <Wrapper>
       <Text>{props.text}</Text>
       <Actions>
-        <ActionBtn onClick={handleCopyToClipboard}><CopySVG /></ActionBtn>
+        <ActionBtn onClick={handleCopyToClipboard}>
+          <CopySVG />
+          <span>Copy</span>
+        </ActionBtn>
+        {isValidURL && (
+          <ActionBtn>
+            <LinkSVG />
+            <span>Open</span>
+          </ActionBtn>
+        )}
       </Actions>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -29,42 +48,74 @@ const Wrapper = styled.div`
   border: 2px solid black;
   border-radius: 15px;
   transition: 0.3s ease;
+  overflow: hidden;
 
   &:hover {
     // Setting Visiblitity of ActionBtn
     div {
-      display: flex;
+      transform: translateX(0);
     }
   }
 `
 const Text = styled.p`
   font-size: 1rem;
   letter-spacing: 1px;
-`
+`;
 const Actions = styled.div`
-  display: none;
+  box-sizing: content-box;
+  position: absolute;
+  right: 0;
+  display: flex;
   gap: 0.75rem;
-`
+  padding-right: 0.75rem;
+  transition: 0.3s ease;
+  transform: translateX(100%);
+`;
 const ActionBtn = styled.button` 
-  background-color: black;
+  display: flex;
+  align-items: center;
+  background-color: white;
   cursor: pointer;
-  height: 2.5rem;
-  width: 2.5rem;
+  min-width: 2.5rem;
   border: 0;
   border-radius: 10px;
-  padding: 0.7rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
 
   svg {
-    fill: white;
+    fill: rgba(0, 0, 0, 0.6);
+    height: 2.5rem;
+    width: 2.5rem;
+    padding: 0.7rem;
+    transition: 0.5s ease-in-out;
+  }
+
+  span {
+    width: 0;
+    padding-left: 0;
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 1rem;
+    font-weight: 550;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    text-align: left;
+    overflow: hidden;
+    transition: 0.5s ease-in-out;
   }
 
   &:hover {
+    background-color: #f5f5f5;
+
     svg {
       transform: scale(1.05);
     }
+    
+    span {
+      box-sizing: content-box;
+      width: 3rem;
+      padding-right: 0.7rem;
+    }
   }
-`
+`;
 
 QrResultDialog.propTypes = {
   text: PropTypes.string.isRequired,
