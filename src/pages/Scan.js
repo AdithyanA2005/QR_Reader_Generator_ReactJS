414a6421df5b62fd0023e62c492fbf81e6fb6ee7
Adithyan A <deepaadithyan56@gmail.com>
2023-04-04 18:41:09 +0530

    Error handle for qrscanner added

--- src/pages/Scan.js
+++ src/pages/Scan.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import QrReader from "react-qr-scanner";
 import PageContainer from "../components/Containers/PageContainer";
 import QrResultDialog from "../components/QrResultDialog";
@@ -8,6 +9,7 @@ import { toast } from "react-toastify";
 
 export default function Scan() {
   const [result, setResult] = useState(null);
+  const [scannerErr, setScannerErr] = useState(null);
 
   const handleOnScan = (data) => {
     if (data != null) {
@@ -20,9 +22,17 @@ export default function Scan() {
       position: "top-center",
       hideProgressBar: false,
     });
+    setScannerErr(null);
   };
   const handleOnError = (err) => {
-    console.error(err)
+    let message = null;
+    if (err.message === "Permission denied") {
+      message = "Please enable camera permission to scan qrcodes";
+    } else if (err.message === "Requested device not found"){
+      message = "No camera found to scan qrcodes"
+    }
+    setScannerErr(message);
+    toast.error(message)
   };
 
   return (
@@ -31,13 +41,17 @@ export default function Scan() {
         {!result ? (
           <>
             <Heading title="Scan QR-Code" />
-            <QrReader
-              delay={500}
-              style={{ maxWidth: "100%", border: "2px solid black" }}
-              onError={handleOnError}
-              onScan={handleOnScan}
-              onLoad={handleOnLoad}
-            />
+            {!scannerErr ? (
+              <QrReader
+                delay={500}
+                style={{ maxWidth: "100%", border: "2px solid black", margin: "auto", display: "block" }}
+                onError={handleOnError}
+                onScan={handleOnScan}
+                onLoad={handleOnLoad}
+              />
+            ) : (
+              <ErrText>{`${scannerErr}, Fix the issue to start scanning or try refreshing the website`} </ErrText>
+            )}
           </>
         ) : (
           <>
@@ -50,3 +64,7 @@ export default function Scan() {
   );
 };
 
+const ErrText = styled.p`
+  font-size: 1.2rem;
+  letter-spacing: 1px;
+`;
